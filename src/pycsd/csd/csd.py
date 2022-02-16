9b3f2ae0b8f3e4776921c68e8ed12cf1de9b4479
Wai-Shing Luk <luk036@gmail.com>
2022-02-16 15:54:16 +0800

    update csd algorithms

--- src/pycsd/csd/csd.py
+++ src/pycsd/csd/csd.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 """
  Canonical Signed Digit Functions
 
@@ -17,7 +16,7 @@
 from math import ceil, fabs, log
 
 
-def to_csd( num, places=0, debug=False ):
+def to_csd(num: float, places: int) -> str:
     """
     Convert the argument `num` to a string in CSD Format.
     Parameters
@@ -35,30 +34,18 @@ def to_csd( num, places=0, debug=False ):
     License: GPL2
     """
 
-    if debug:
-        print("Converting %f " % (num), )
-
     # figure out binary range, special case for 0
-    if num == 0:
-        return '0'
-
+    if num == 0.0:
+        return "0"
+    
     absnum = fabs(num)
-    n = 0 if absnum < 1 else ceil(log(absnum * 1.5, 2))
-    csd_str = '0' if absnum < 1 else ''
-
-    if debug:
-        print("to %d.%d format" % (n, places))
-
-    # limit = pow(2., n) / 3.
+    n = 0 if absnum < 1.0 else cell(log(absnum * 1.5, 2))
+    csd_str = "0" if absnum < 1.0 else ""
     pow2n = pow(2.0, n - 1)
-    while (n > -places):
-        if debug:
-            print("  ", num, pow2n / 1.5)
-
-        # decimal point?
+    while n > -places:
         if n == 0:
             csd_str += '.'
-
+        
         n -= 1
         # convert the number
         d = 1.5 * num
@@ -70,65 +57,49 @@ def to_csd( num, places=0, debug=False ):
             num += pow2n
         else:
             csd_str += '0'
+        
         pow2n /= 2.0
-
-        if debug:
-            print(csd_str)
-
+    
     return csd_str
 
 
-def to_decimal( csd_str, debug=False ):
+def to_decimal(csd_str: str) -> float:
     """ Convert the CSD string to a decimal """
 
-    if debug:
-        print("Converting: ", csd_str)
-
-    num = 0.0
-    loc = 0
+    num: float = 0.0
+    loc: int = 0
     for i, c in enumerate(csd_str):
-        if c == '.':
+        if c == '0':
+            num *= 2.0
+        elif c == '+':
+            num = num * 2.0 + 1.0
+        elif c == '-':
+            num = num * 2.0 - 1.0
+        elif c == '.':
             loc = i + 1
         else:
-            num *= 2
-            if c != '0':
-                if c == '+':
-                    num += 1
-                elif c == '-':
-                    num -= 1
-                else:
-                    raise ValueError
+            raise ValueError
+    
     if loc != 0:
-        num /= pow(2, len(csd_str) - loc)
+        num /= pow(2.0, len(csd_str) - loc)
+
     return num
 
 
-def to_csdfixed(num, nnz=4, debug=False):
+def to_csdfixed(num: float, nnz: int) -> str:
     """ Convert the argument to CSD Format. """
 
-    if debug:
-        print(
-            "Converting %f " % (num),
-        )
+    if num == 0.0:
+        return "0"
 
-    # figure out binary range, special case for 0
-    if num == 0:
-        return '0'
     absnum = fabs(num)
-    n = 0 if absnum < 1 else ceil(log(absnum * 1.5, 2))
-    csd_str = '0' if absnum < 1 else ''
-    # limit = pow(2., n) / 3.
-    pow2n = pow(2, n - 1)
+    n = 0 if absnum < 1.0 else cell(log(absnum * 1.5, 2))
+    csd_str = "0" if absnum < 1.0 else ""
+    pow2n = pow(2.0, n - 1)
     while n > 0 or (nnz > 0 and fabs(num) > 1e-100):
-        if debug:
-            print("  ", num, pow2n / 1.5 )
-
-        # decimal point?
         if n == 0:
             csd_str += '.'
-
         n -= 1
-        # convert the number
         d = 1.5 * num
         if d > pow2n:
             csd_str += '+'
@@ -140,12 +111,19 @@ def to_csdfixed(num, nnz=4, debug=False):
             nnz -= 1
         else:
             csd_str += '0'
-        pow2n /= 2.0  # python 2.X needs tbe dot
-
+        
+        pow2n /= 2.0
         if nnz == 0:
-            num = 0
-
-        if debug:
-            print(csd_str)
-
+            num = 0.0
     return csd_str
+
+
+if __name__ == "main":
+    assert to_csd(28.5, 2) == "+00-00.+0"
+    assert to_csd(-0.5, 2) == "0.-0"
+
+    assert to_decimal("+00-00.+") == 28.5
+    assert to_decimal("0.-") == -0.5
+
+    assert to_csdfixed(28.5, 4) == "+00-00.+"
+    assert to_csdfixed(-0.5, 4) == "0.-"
