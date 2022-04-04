638cd701fb1374218a3578e618f63d2520a4ad45
Wai-Shing Luk <luk036@gmail.com>
2022-04-04 15:16:05 +0800

    apply format

--- src/pycsd/csd/csd.py
+++ src/pycsd/csd/csd.py
@@ -37,49 +37,49 @@ def to_csd(num: float, places: int) -> str:
     # figure out binary range, special case for 0
     if num == 0.0:
         return "0"
-    
+
     absnum = fabs(num)
     n = 0 if absnum < 1.0 else ceil(log(absnum * 1.5, 2))
     csd_str = "0" if absnum < 1.0 else ""
     pow2n = pow(2.0, n - 1)
     while n > -places:
         if n == 0:
-            csd_str += '.'
-        
+            csd_str += "."
+
         n -= 1
         # convert the number
         d = 1.5 * num
         if d > pow2n:
-            csd_str += '+'
+            csd_str += "+"
             num -= pow2n
         elif d < -pow2n:
-            csd_str += '-'
+            csd_str += "-"
             num += pow2n
         else:
-            csd_str += '0'
-        
+            csd_str += "0"
+
         pow2n /= 2.0
-    
+
     return csd_str
 
 
 def to_decimal(csd_str: str) -> float:
-    """ Convert the CSD string to a decimal """
+    """Convert the CSD string to a decimal"""
 
     num: float = 0.0
     loc: int = 0
     for i, c in enumerate(csd_str):
-        if c == '0':
+        if c == "0":
             num *= 2.0
-        elif c == '+':
+        elif c == "+":
             num = num * 2.0 + 1.0
-        elif c == '-':
+        elif c == "-":
             num = num * 2.0 - 1.0
-        elif c == '.':
+        elif c == ".":
             loc = i + 1
         else:
             raise ValueError
-    
+
     if loc != 0:
         num /= pow(2.0, len(csd_str) - loc)
 
@@ -87,7 +87,7 @@ def to_decimal(csd_str: str) -> float:
 
 
 def to_csdfixed(num: float, nnz: int) -> str:
-    """ Convert the argument to CSD Format. """
+    """Convert the argument to CSD Format."""
 
     if num == 0.0:
         return "0"
@@ -98,20 +98,20 @@ def to_csdfixed(num: float, nnz: int) -> str:
     pow2n = pow(2.0, n - 1)
     while n > 0 or (nnz > 0 and fabs(num) > 1e-100):
         if n == 0:
-            csd_str += '.'
+            csd_str += "."
         n -= 1
         d = 1.5 * num
         if d > pow2n:
-            csd_str += '+'
+            csd_str += "+"
             num -= pow2n
             nnz -= 1
         elif d < -pow2n:
-            csd_str += '-'
+            csd_str += "-"
             num += pow2n
             nnz -= 1
         else:
-            csd_str += '0'
-        
+            csd_str += "0"
+
         pow2n /= 2.0
         if nnz == 0:
             num = 0.0
